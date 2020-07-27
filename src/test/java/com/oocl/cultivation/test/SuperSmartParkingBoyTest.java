9b1204d1126fa4efebba7aff1e749b69c426422f
LISH5 <727904507@qq.com>
2020-07-27 13:00:18 +0800

    [ParkingLot][ShaoLi] refactor: modify should_add_car_into_parkinglot_has_larger_rate_when_park_by_supersmartparkingboy_given_car
    simplify the case

--- src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -14,33 +14,18 @@ public class SuperSmartParkingBoyTest {
     void should_add_car_into_parkinglot_has_larger_rate_when_park_by_supersmartparkingboy_given_car() {
         //given
         ParkingLot parkingLotWith5Room = new ParkingLot(5);
-        parkingLotWith5Room.setName("parkingLotWith5Room");
         ParkingLot parkingLotWith10Room = new ParkingLot(10);
-        parkingLotWith10Room.setName("parkingLotWith10Room");
-        ParkingLot parkingLotWith15Room = new ParkingLot(15);
-        parkingLotWith15Room.setName("parkingLotWith15Room");
         SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(
-                Arrays.asList(parkingLotWith5Room, parkingLotWith10Room, parkingLotWith15Room)
+                Arrays.asList(parkingLotWith5Room, parkingLotWith10Room)
         );
-        StringBuilder maxRoomParkingLotList = new StringBuilder();
 
         //when
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             superSmartParkingBoy.park(new Car());
-            maxRoomParkingLotList.append(superSmartParkingBoy.getMaxRoomParkingLot().toString());
         }
 
         //then
-        assertEquals("name: parkingLotWith5Room, remainRooms: 4;\n" +
-                "name: parkingLotWith10Room, remainRooms: 9;\n" +
-                "name: parkingLotWith15Room, remainRooms: 14;\n" +
-                "name: parkingLotWith15Room, remainRooms: 13;\n" +
-                "name: parkingLotWith10Room, remainRooms: 8;\n" +
-                "name: parkingLotWith15Room, remainRooms: 12;\n" +
-                "name: parkingLotWith5Room, remainRooms: 3;\n" +
-                "name: parkingLotWith10Room, remainRooms: 7;\n" +
-                "name: parkingLotWith15Room, remainRooms: 11;\n" +
-                "name: parkingLotWith15Room, remainRooms: 10;\n", maxRoomParkingLotList.toString());
-
+        assertEquals(3, parkingLotWith5Room.getNowRemainRoom());
+        assertEquals(7, parkingLotWith10Room.getNowRemainRoom());
     }
 }
