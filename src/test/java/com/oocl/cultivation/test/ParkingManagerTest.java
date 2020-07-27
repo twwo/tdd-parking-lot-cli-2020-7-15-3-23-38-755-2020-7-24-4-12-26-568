5f1c2b5292263148798828f8640e645d4336a345
LISH5 <727904507@qq.com>
2020-07-27 19:12:28 +0800

    [ParkingLot Story6][ShaoLi] test: add and pass should_specify_supersmartparkingboy_park_the_car_when_park_given_car

--- src/test/java/com/oocl/cultivation/test/ParkingManagerTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingManagerTest.java
@@ -41,4 +41,16 @@ public class ParkingManagerTest {
         //then
         assertNotNull(CarTicket);
     }
+
+    @Test
+    void should_specify_supersmartparkingboy_park_the_car_when_park_given_car() {
+        //given
+        ParkingManager parkingManager = new ParkingManager(new SuperSmartParkingBoy(new ParkingLot(1)));
+
+        //when
+        CarTicket CarTicket = (CarTicket) parkingManager.park(new Car()).getResult();
+
+        //then
+        assertNotNull(CarTicket);
+    }
 }
