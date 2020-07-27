348411f09c9495095c19bc919f30a63434ab1c35
LISH5 <727904507@qq.com>
2020-07-27 17:51:31 +0800

    [ParkingLot][ShaoLi] refactor: delete maxRoomParkingLot and useless constructor, modify methods park and getMaxRemainRoomParkingLot

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -3,29 +3,24 @@ package com.oocl.cultivation;
 import java.util.List;
 
 public class SmartParkingBoy extends ParkingBoy {
-    private ParkingLot maxRoomParkingLot;
-
-    public SmartParkingBoy() {
-        maxRoomParkingLot = getMaxRemainRoomParkingLot();
-    }
 
     public SmartParkingBoy(ParkingLot...parkingLots) {
         super(parkingLots);
-        maxRoomParkingLot = getMaxRemainRoomParkingLot();
     }
 
     @Override
     public ReturnResult park(Car car) {
         ReturnResult returnResult = new ReturnResult();
         returnResult.setMessage(FetchOrParkMessage.NO_PARKING_POSITION.toString());
-        if (!getMaxRemainRoomParkingLot().isParkingLotFull()) {
-            returnResult = getMaxRemainRoomParkingLot().park(car);
+        ParkingLot maxRemainRoomParkingLot = getMaxRemainRoomParkingLot();
+        if (!maxRemainRoomParkingLot.isParkingLotFull()) {
+            returnResult = maxRemainRoomParkingLot.park(car);
         }
         return returnResult;
     }
 
     public ParkingLot getMaxRemainRoomParkingLot() {
-        maxRoomParkingLot = super.getParkingLots().get(0);
+        ParkingLot maxRoomParkingLot = super.getParkingLots().get(0);
         for (ParkingLot parkingLot : super.getParkingLots()) {
             if (parkingLot.getNowRemainRoom() > maxRoomParkingLot.getNowRemainRoom()) {
                 maxRoomParkingLot = parkingLot;
